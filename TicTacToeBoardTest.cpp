9e218f8b05d50808873d3d480e18392e6de6deb1
student <student@softeng>
2017-02-27 11:37:03 -0800

    Added few tests to TicTacToeTest.cpp

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,7 +14,21 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+TicTacToeBoard tp;
+
 TEST(TicTacToeBoardTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(TicTacToeBoardTest, boardCheck)
+{
+	tp.placePiece(4,4);
+	ASSERT_TRUE(Invalid);
+}
+
+TEST(TicTacToeBoardTest, placeCheck)
+{
+	tp.placePiece(0,2);
+	ASSERT_TRUE(X);
 }
